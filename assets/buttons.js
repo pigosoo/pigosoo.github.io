fecfaadd918a6a8e4e7534eedd9ded605f13cadf
pigosoo <60561143+pigosoo@users.noreply.github.com>
2020-02-01 22:18:58 -0500

    Main files

---
+++ assets/buttons.js
@@ -0,0 +1,11 @@
+function emailFunction() {
+	window.open('mailto:tony.afsun@gmail.com');
+}
+
+function resumeFunction() {
+	window.open('assets/resume.pdf');
+}
+
+function cvFunction() {
+	window.open('assets/CurrVit.pdf');
+}
